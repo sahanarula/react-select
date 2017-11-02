1e796fdbd3798cda1107e39f8543255c242ab540
Sahil Narula <sahiln123@gmail.com>
2017-11-02 17:58:03 +0530

    fixed dragging issue

--- dist/rselector.js
+++ dist/rselector.js
@@ -750,6 +750,8 @@ var Option = (function (_React$Component) {
 		this.handleMouseDown = this.handleMouseDown.bind(this);
 		this.handleMouseEnter = this.handleMouseEnter.bind(this);
 		this.handleMouseMove = this.handleMouseMove.bind(this);
+		this.handleTouchEnd = this.handleTouchEnd.bind(this);
+		this.handleTouchMove = this.handleTouchMove.bind(this);
 		this.handleTouchStart = this.handleTouchStart.bind(this);
 		this.onFocus = this.onFocus.bind(this);
 	}
